2e8c90a641b630c84698a1d733a52f315e490048
Muhammad Razif <razifnations@gmail.com>
2023-10-19 11:01:18 +0700

    Mapping List

---
+++ mapping_list/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "Name": "Muhammad Razif",
+      "Age": 20,
+      "favColor": [
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber"
+      ],
+    },
+    {
+      "Name": "Mautya",
+      "Age": 18,
+      "favColor": ["Yellow", "Red", "Amber"],
+    },
+    {
+      "Name": "Ajip",
+      "Age": 20,
+      "favColor": [
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber"
+      ],
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ID Apps"),
+        ),
+        body: ListView(
+          children: myList.map((data) {
+            print(data["favColor"]);
+            List myfavColor = data["favColor"];
+            print("------------");
+            print(myfavColor);
+            return Card(
+              margin: EdgeInsets.all(20),
+              color: Colors.black.withOpacity(0.1),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name: ${data['Name']}"),
+                            Text("Age: ${data['Age']}"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    //favcolor
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: myfavColor.map((color) {
+                          return Container(
+                            color: Colors.pink,
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 8,
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(color),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
