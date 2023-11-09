130930f78fc8c5bd6a906bb146393ff347f0c7cf
Muhammad Razif <razifnations@gmail.com>
2023-11-09 17:28:46 +0700

    Drawer

---
+++ drawer/lib/pages/page_satu.dart
@@ -0,0 +1,87 @@
+import 'package:drawer/pages/setting_page.dart';
+import 'package:flutter/material.dart';
+
+class PageSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Satu"),
+
+        // leading: Icon(Icons.menu),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 150,
+              color: Colors.red,
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Menu Pilihan",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => PageSatu(),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.home,
+                size: 35,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => SettingPage(),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.settings,
+                size: 35,
+              ),
+              title: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "Some Text Here",
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
